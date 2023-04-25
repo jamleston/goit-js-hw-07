1eb0116d18de662a0815e4b1cae84de5e0bcf8bf
jamleston <jamlessston@gmail.com>
2023-04-25 09:36:12 +0200

    Start

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// refs and const
+const refs = {
+    galleryListRef: document.querySelector('.gallery')
+}
+
+const itemsMarkup = createArrayOfItems(galleryItems);
+
+// events
+
+refs.galleryListRef.insertAdjacentHTML("afterbegin", itemsMarkup);
+refs.galleryListRef.addEventListener('click', onOpenModal);
+
+// functions
+
+function createArrayOfItems (array) {
+    return array.map(elem =>{
+        return `<li class="gallery__item">
+        <a class="gallery__link" href=${elem.original}>
+        <img
+            class="gallery__image"
+            src=${elem.preview}
+            data-source=${elem.original}
+            alt=${elem.description}
+        />
+        </a>
+        </li>`
+    }).join('');
+};
+
+function onOpenModal (event) {
+    event.preventDefault();
+    const currentItem = event.target;
+
+    if(currentItem.nodeName !=='IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img class='gallery__image' data-src=${currentItem.parentNode.href} width="800" height="600">`)
+
+    instance.show();
+    console.log(instance);
+}
